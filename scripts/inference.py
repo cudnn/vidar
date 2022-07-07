13f438cf183bacbd503dd3bd1bf8d1801322e934
root <root@RD-T4-Spot-2.5kx353s3zybudk2et5zqwifakd.ax.internal.cloudapp.net>
2022-07-07 12:30:00 +0000

    Added try/except for image size, and log messages

--- scripts/inference.py
+++ scripts/inference.py
@@ -18,6 +18,30 @@ from vidar.utils.config import read_config
 
 from pdb import set_trace as breakpoint
 
+class Log:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    @staticmethod
+    def warning(s):
+        print(Log.WARNING + '[Warning] ' + s + Log.ENDC)
+
+    @staticmethod
+    def error(s):
+        print(Log.ERROR + '[ERROR] ' + s + Log.ENDC)
+
+    @staticmethod
+    def info(s):
+        print(Log.OKBLUE + '[INFO] ' + s + Log.ENDC)
+
+
 def upper_power_of_2(x):
     l = torch.log2(x)
     f = torch.floor(l)
@@ -29,7 +53,8 @@ def pad_to_upper_power_of_2(image):
     delta_size = ((padded_size - image_size) / 2).int().flip(dims=[0]) # Note: pb if the /2 is odd, one dim will be a power of 2 minus 1, but it appears to work with the network anyway so...
 
     pad_transform = T.Pad(padding=delta_size.tolist())
-    return pad_transform(image)
+    return pad_transform(image
+            )
 
 def resize_image_to_tensor(image, base_size):
     """
@@ -45,8 +70,9 @@ def resize_image_to_tensor(image, base_size):
 
     image_size = torch.Tensor([image.size[1], image.size[0]])
     
-    if (image_size - base_size).any() <= 7: # The image is smaller than the base_size
-        print(f'Warning : Image of size {image_size} is smaller than base_size {base_size} and will be stretched to match it. JK nothing will be done about it because I don\'t know if it is needed.')
+    if (image_size - base_size <= 7).any(): # The image is smaller than the base_size, 7 is the biggest kernel size
+        Log.warning(f'Warning : Image of size {image_size} is smaller than base_size {base_size} and will be stretched to match it.')
+        image = resize_pil(image, tuple(base_size.int().tolist()))
         return to_tensor_image(image)[:3] # Remove alpha channel if needed
 
     else: # The image is larger than the base_size
@@ -54,7 +80,7 @@ def resize_image_to_tensor(image, base_size):
         return image[:, :image_size[0]//base_size[0] * base_size[0], :image_size[1]//base_size[1] * base_size[1]][:3] # Remove alpha channel
 
 
-def infer(cfg, checkpoint, input, output, **kwargs):
+def infer(cfg, checkpoint, input_path, output_path, **kwargs):
     """
     Runs an inference with the given config file or checkpoint.
     """
@@ -63,13 +89,13 @@ def infer(cfg, checkpoint, input, output, **kwargs):
 
     cfg = read_config(cfg, **kwargs)
 
-    wrapper = Wrapper(cfg, verbose=True)
-    wrapper.load(checkpoint, verbose=True)
+    wrapper = Wrapper(cfg, verbose=False)
+    wrapper.load(checkpoint, verbose=False)
     wrapper.eval_custom()
 
     #wrapper.evaluate(batch, output)
 
-    if os.path.isdir(input):
+    if os.path.isdir(input_path):
         # If input file is a folder, search for image files
         files = []
         for ext in ['png', 'jpg']:
@@ -78,38 +104,31 @@ def infer(cfg, checkpoint, input, output, **kwargs):
         print('Found {} files'.format(len(files)))
     else:
         # Otherwise, use it as is
-        files = [input]
+        files = [input_path]
 
     # Process each file
     for input_file in files:
 
         # Load image
-        #image = read_image(input_file) # load as tensor
         image = Image.open(input_file) # load as PIL image
 
-        # Resize and to tensor
-        #image = pad_to_upper_power_of_2(image)
-        #print("#### AFTER PADDING", image.shape)
-        
-        # Handling frame size
-        base_shape = torch.Tensor([192, 640])
-        #shape = (2048, 2048)
-        
-        image = resize_image_to_tensor(image, base_shape)
-        #image = resize_image(image, image_shape)
-        #image = to_tensor(image).unsqueeze(0)
+        # Try with current image size
+        try:
+            output = wrapper.run_arch({'rgb': to_tensor_image(image)[(None,)*2]}, 0, False, False)
+        except:
+            Log.warning('Warning : image size is not compatible with the network. It will be resized to a compatible size (the closest possible to the original size)')
+            # Resizing image to a size that's known to work
+            base_shape = torch.Tensor([192, 640])
+            image = resize_image_to_tensor(image, base_shape)
 
-        # Contains multiple batches
-        batches = {
-            'rgb': image.unsqueeze(0).unsqueeze(0) # First image of the first batch
-        }
+            # Then computing output
+            output = wrapper.run_arch({'rgb': image[(None,)*2]}, 0, False, False)
 
-    
-        output = wrapper.run_arch(batches, 0, False, False)
-       
+
         depth_map = output['predictions']['depth'][0][0]
         depth_map /= depth_map.max()
-        save_image(depth_map, '/data/output.png')
+        save_image(depth_map, output_path)
+        Log.info(f'Depth map inference done, saved depth map at {output_path}')
 
 
 if __name__ == '__main__':
