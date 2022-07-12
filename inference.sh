36e1f931eb0f9c21946f715cf086aca90af980cc
root <root@RD-T4-Spot-2.5kx353s3zybudk2et5zqwifakd.ax.internal.cloudapp.net>
2022-07-12 13:25:34 +0000

    Autosave

--- inference.sh
+++ inference.sh
@@ -1,1 +1,2 @@
+git pull
 python3 ./scripts/inference/inference.py ./configs/papers/selfcalib/ucm_kitti.yaml /data/vidar/checkpoints/good-microwave-6/models/050.ckpt bru_it_got_folded.mp4 /data/output/
