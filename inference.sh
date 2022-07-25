2a4fa3acd5890777070e662a6d02a32783ac2f34
Theo-Cheynel <theo.cheynel@gmail.com>
2022-07-25 13:59:12 +0000

    Added config without dataset for inference

--- inference.sh
+++ inference.sh
@@ -1,2 +1,2 @@
 git pull
-python3 ./scripts/inference/inference.py ./configs/papers/selfcalib/ucm_kitti.yaml /data/vidar/checkpoints/good-microwave-6/models/050.ckpt bru_it_got_folded.mp4 /data/output/
+python3 ./scripts/inference/inference.py ./configs/papers/selfcalib/ucm_kitti_inference.yaml /data/vidar/checkpoints/good-microwave-6/models/050.ckpt bru_it_got_folded.mp4 /data/output/
